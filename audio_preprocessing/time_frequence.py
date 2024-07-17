ad98b750f17983092502b1fbdb6d5c49fc28e0d6
dominic.bechtold <dominic.bechtold@icloud.com>
2024-07-17 21:12:57 +0200

    Normalized t/f py

--- audio_preprocessing/time_frequence.py
+++ audio_preprocessing/time_frequence.py
@@ -2,14 +2,34 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np 
+import scipy.signal
+
 
 audio_file1 = 'audio_preprocessing/audios/audio_to_prepare.mp3'
 audio_file2 = 'audio_preprocessing/audios/screaming+song.mp3'
 audio_file3 = 'audio_preprocessing/audios/talk_1.wav'
 audio_file4 = 'audio_preprocessing/audios/talk_2.wav'
 
+
+def highpass_filter(y, sr, cutoff_freq=50):
+    # Design a Butterworth highpass filter
+    sos = scipy.signal.iirfilter(
+        N=2,
+        Wn=cutoff_freq,
+        btype='highpass',
+        output='sos',
+        fs=sr
+    )
+    y_filtered = scipy.signal.sosfilt(sos, y)
+    return y_filtered
+
+
 def Audio_Frequence(audio):
     y, sr = librosa.load(audio)
+    y = librosa.util.normalize(y)
+    # y = (y - np.min(y)) / (np.max(y) - np.min(y))
+
+    y = highpass_filter(y, sr)
 
     spec = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
 
@@ -19,10 +39,9 @@ def Audio_Frequence(audio):
     plt.title('Spektrogramm der Audio')
     plt.xlabel('Zeit (s)')
     plt.ylabel('Frequenz (Hz)')
-    plt.tight_layout()
-
     plt.savefig(fname='audio_preprocessing/spektrogramm', bbox_inches='tight')
     plt.show()
+
 
 Audio_Frequence(audio_file1)
 Audio_Frequence(audio_file2) 
