c18bb4ac56bc3e347d2bf54a619642f324adf002
dominic.bechtold <dominic.bechtold@icloud.com>
2024-07-17 16:49:50 +0200

    main

--- python_mathematic_algorithm/short_time_fourier_transformation/display_stft.py
+++ python_mathematic_algorithm/short_time_fourier_transformation/display_stft.py
@@ -4,23 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
 
-def plot_spectrogram_random_audio(audio_signal:np.ndarray = 0, fs: int=1000, duration: float=5.0,
-                                  freq:int=50, window:str='hamming', nperseg:int=256, noverlap:int=128):
 
-    if audio_signal == 0:
-        t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-        audio_signal_ex = np.sin(2 * np.pi * freq * t)  
-        # Füge Rauschen hinzu (optional)
-        # audio_signal_ex += 0.0005 * np.random.randn(len(audio_signal_ex))
-        # Berechne die STFT
-        frequencies, times, spectrogram_data = spectrogram(audio_signal_ex, fs=fs, window=window,
-                                                            nperseg=nperseg, noverlap=noverlap)
-    else:
-        frequencies, times, spectrogram_data = spectrogram(audio_signal, fs=fs, window=window,
-                                                           nperseg=nperseg, noverlap=noverlap)
+def plot_spectrogram(audio_signal: np.ndarray, fs: int=1000, window: str='hamming',
+                     nperseg: int=256, noverlap: int=128):
+    
+    nperseg = min(nperseg, len(audio_signal))
+    # Sicherstellen, dass noverlap kleiner als nperseg ist
+    noverlap = min(noverlap, nperseg - 1)
+    frequencies, times, spectrogram_data = spectrogram(audio_signal, fs=fs, window=window,
+                                                       nperseg=nperseg, noverlap=noverlap)
+    
     # Plot des Spektrogramms
     plt.figure(figsize=(10, 4))
-    plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram_data))  
+    plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram_data))
     plt.title('Spektrogramm')
     plt.ylabel('Frequenz (Hz)')
     plt.xlabel('Zeit (s)')
@@ -28,11 +24,22 @@ def plot_spectrogram_random_audio(audio_signal:np.ndarray = 0, fs: int=1000, dur
     plt.tight_layout()
     plt.show()
 
-if __name__ == '__main__':
-    plot_spectrogram_random_audio()
 
-    # TODO: Implement randomdata fft into plotting
- 
-    randomdata = np.random.randint(0, 100, 100)
-    fft_data = FastFourierTransformation(randomdata)
-    # plot_spectrogram_random_audio(audio_signal=fft_data)
+def plot_spectrogram_random(fs: int=1000, duration: float=5.0, freq: int=50, window: str='hamming',
+                                  nperseg: int=256, noverlap: int=128):
+    t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+    audio_signal = np.sin(2 * np.pi * freq * t)  
+    # Füge Rauschen hinzu (optional)
+    audio_signal += 0.0005 * np.random.randn(len(audio_signal))
+    
+    plot_spectrogram(audio_signal, fs, window, nperseg, noverlap)
+
+
+if __name__ == '__main__':
+    plot_spectrogram_random()
+
+    randomdata = np.random.randint(-1, 1, 2000)     # Close call to "real" audio files 
+    fft_data = np.abs(FastFourierTransformation_u_scipy(randomdata))
+    normalize = fft_data / np.max(fft_data)
+
+    plot_spectrogram(normalize)
--- python_mathematic_algorithm/short_time_fourier_transformation/fourier_transformation.py
+++ python_mathematic_algorithm/short_time_fourier_transformation/fourier_transformation.py
@@ -1,14 +1,27 @@
 import numpy as np
-from scipy.fft import fft 
+import scipy.fft
 
 
-def FastFourierTransformation(data: np.ndarray):
-    fft_data = fft(data)
+def FastFourierTransformation_u_scipy(data: np.ndarray):
+    fft_data = scipy.fft.fft(data)
+
+    return fft_data
+
+
+def FastFourierTransformation_u_numpy(data: np.ndarray):
+    fft_data = np.fft.fft(data)
 
     return fft_data
 
 if __name__ == '__main__':
     randomdata = np.random.randint(0, 100, 100)
-    fft_data = FastFourierTransformation(randomdata)
+    
+    fft_data = FastFourierTransformation_u_scipy(randomdata)
     print('Data\n', randomdata)
-    print('FFT Data \n', fft_data)
+    print('FFT Data w/ scipy \n', fft_data)
+
+    fft_data_np = FastFourierTransformation_u_numpy(randomdata)
+    print('Data\n', randomdata)
+    print('FFt Data w/ np:\n', fft_data_np)
+
+    print(np.allclose(fft_data_np, fft_data))       # np.allclose checking with tolerance
